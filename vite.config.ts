dc3e3770433e68ebc530c62b3f6977af67860d3b
sharma6754 <151129641+sharma6754@users.noreply.github.com>
2025-02-10 12:52:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/todo-app/", // Replace 'todo-app' with your repo name
+  base: "/TBD-UpNext/",
   plugins: [react()],
 });
